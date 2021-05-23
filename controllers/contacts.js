21afc67bfbaf0622d30841d41094ed5d52e50457
Gurhan Kokcu <gurhankokcu@gmail.com>
2021-05-24 00:43:23 +0100

    search contacts by name phone and email

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,9 +1,46 @@
 'use strict'
 
+const config = require('config')
+const Joi = require('joi')
+const Op = require('sequelize').Op
 const { Contact } = require('../models')
 
+const querySchema = Joi.object({
+  page: Joi.number().min(1).default(1),
+  operator: Joi.string().valid('and', 'or').default('and'),
+  name: Joi.string().pattern(/^[a-zA-Z0-9]{1,30}$/),
+  phone: Joi.string().pattern(/^[0-9]{1,30}$/),
+  email: Joi.string().pattern(/^[a-zA-Z0-9@.+_-]{1,30}$/)
+})
+
 async function listContacts (req, res, next) {
-  const contacts = await Contact.findAll()
+  const { value, error } = querySchema.validate(req.query)
+  if (error) {
+    return res.json({
+      error: true,
+      message: 'Validation error!',
+      details: error.message
+    })
+  }
+
+  const pagesize = config.get('pagesize')
+
+  const where = []
+  if (value.name) {
+    where.push({ name: { [Op.like]: `%${value.name}%` } })
+  }
+  if (value.phone) {
+    where.push({ phone: { [Op.like]: `%${value.phone}%` } })
+  }
+  if (value.email) {
+    where.push({ email: { [Op.like]: `%${value.email}%` } })
+  }
+
+  const contacts = await Contact.findAll({
+    where: { [Op[value.operator]]: where },
+    offset: (value.page - 1) * pagesize,
+    limit: pagesize
+  })
   res.json(contacts)
 }
 
--- tests/controllers/contacts.test.js
+++ tests/controllers/contacts.test.js
@@ -11,20 +11,166 @@ describe('contacts controller', () => {
     }]
     const mockModels = {
       Contact: {
-        findAll: jest.fn().mockImplementation(() => contactsData)
+        findAll: () => {}
       }
     }
+    const mockReq = { query: {} }
+    const mockRes = { json: () => {} }
 
     beforeEach(() => {
+      jest.mock('config', () => ({
+        get: jest.fn().mockImplementation((key) => key === 'pagesize' ? 5 : 99)
+      }))
       jest.mock('../../models', () => mockModels)
+      jest.spyOn(mockModels.Contact, 'findAll').mockImplementation(() => contactsData)
+      mockReq.query = {}
+      jest.spyOn(mockRes, 'json')
     })
 
     it('should return all contacts', async () => {
-      const controller = require('../../controllers/contacts')
-      const mockRes = { json: jest.fn() }
-      await controller.listContacts(null, mockRes)
+      await require('../../controllers/contacts').listContacts(mockReq, mockRes)
       expect(mockRes.json.mock.calls.length).toBe(1)
       expect(mockRes.json.mock.calls[0][0]).toBe(contactsData)
+    })
+
+    it('should return 1st page if page is not specified', async () => {
+      await require('../../controllers/contacts').listContacts(mockReq, mockRes)
+      expect(mockModels.Contact.findAll.mock.calls.length).toBe(1)
+      expect(mockModels.Contact.findAll.mock.calls[0][0].offset).toBe(0)
+      expect(mockModels.Contact.findAll.mock.calls[0][0].limit).toBe(5)
+    })
+
+    it('should return error if page is not a positive number', async () => {
+      mockReq.query.page = -5
+      await require('../../controllers/contacts').listContacts(mockReq, mockRes)
+      expect(mockRes.json.mock.calls.length).toBe(1)
+      expect(mockRes.json.mock.calls[0][0].error).toBe(true)
+      expect(mockRes.json.mock.calls[0][0].message).toBe('Validation error!')
+      expect(mockRes.json.mock.calls[0][0].details).toContain('page')
+    })
+
+    it('should return error if page is not a number', async () => {
+      mockReq.query.page = 'text'
+      await require('../../controllers/contacts').listContacts(mockReq, mockRes)
+      expect(mockRes.json.mock.calls.length).toBe(1)
+      expect(mockRes.json.mock.calls[0][0].error).toBe(true)
+      expect(mockRes.json.mock.calls[0][0].message).toBe('Validation error!')
+      expect(mockRes.json.mock.calls[0][0].details).toContain('page')
+    })
+
+    it('should return 1st page', async () => {
+      mockReq.query.page = 1
+      await require('../../controllers/contacts').listContacts(mockReq, mockRes)
+      expect(mockModels.Contact.findAll.mock.calls.length).toBe(1)
+      expect(mockModels.Contact.findAll.mock.calls[0][0].offset).toBe(0)
+      expect(mockModels.Contact.findAll.mock.calls[0][0].limit).toBe(5)
+    })
+
+    it('should return 2nd page', async () => {
+      mockReq.query.page = 2
+      await require('../../controllers/contacts').listContacts(mockReq, mockRes)
+      expect(mockModels.Contact.findAll.mock.calls.length).toBe(1)
+      expect(mockModels.Contact.findAll.mock.calls[0][0].offset).toBe(5)
+      expect(mockModels.Contact.findAll.mock.calls[0][0].limit).toBe(5)
+    })
+
+    it('should return 3rd page', async () => {
+      mockReq.query.page = '3'
+      await require('../../controllers/contacts').listContacts(mockReq, mockRes)
+      expect(mockModels.Contact.findAll.mock.calls.length).toBe(1)
+      expect(mockModels.Contact.findAll.mock.calls[0][0].offset).toBe(10)
+      expect(mockModels.Contact.findAll.mock.calls[0][0].limit).toBe(5)
+    })
+
+    it('should return error if name query contains non alphanumeric character', async () => {
+      mockReq.query.name = 'na"me'
+      await require('../../controllers/contacts').listContacts(mockReq, mockRes)
+      expect(mockRes.json.mock.calls.length).toBe(1)
+      expect(mockRes.json.mock.calls[0][0].error).toBe(true)
+      expect(mockRes.json.mock.calls[0][0].message).toBe('Validation error!')
+      expect(mockRes.json.mock.calls[0][0].details).toContain('name')
+    })
+
+    it('should query by name', async () => {
+      const Op = require('sequelize').Op
+      mockReq.query.name = 'name'
+      await require('../../controllers/contacts').listContacts(mockReq, mockRes)
+      expect(mockModels.Contact.findAll.mock.calls.length).toBe(1)
+      expect(mockModels.Contact.findAll.mock.calls[0][0].where).toEqual({
+        [Op.and]: [{ name: { [Op.like]: '%name%' } }]
+      })
+    })
+
+    it('should return error if phone query contains non numeric character', async () => {
+      mockReq.query.phone = '32a'
+      await require('../../controllers/contacts').listContacts(mockReq, mockRes)
+      expect(mockRes.json.mock.calls.length).toBe(1)
+      expect(mockRes.json.mock.calls[0][0].error).toBe(true)
+      expect(mockRes.json.mock.calls[0][0].message).toBe('Validation error!')
+      expect(mockRes.json.mock.calls[0][0].details).toContain('phone')
+    })
+
+    it('should query by phone', async () => {
+      const Op = require('sequelize').Op
+      mockReq.query.phone = '321'
+      await require('../../controllers/contacts').listContacts(mockReq, mockRes)
+      expect(mockModels.Contact.findAll.mock.calls.length).toBe(1)
+      expect(mockModels.Contact.findAll.mock.calls[0][0].where).toEqual({
+        [Op.and]: [{ phone: { [Op.like]: '%321%' } }]
+      })
+    })
+
+    it('should return error if email query contains non alphanumeric character', async () => {
+      mockReq.query.email = 'ema"il'
+      await require('../../controllers/contacts').listContacts(mockReq, mockRes)
+      expect(mockRes.json.mock.calls.length).toBe(1)
+      expect(mockRes.json.mock.calls[0][0].error).toBe(true)
+      expect(mockRes.json.mock.calls[0][0].message).toBe('Validation error!')
+      expect(mockRes.json.mock.calls[0][0].details).toContain('email')
+    })
+
+    it('should query by email', async () => {
+      const Op = require('sequelize').Op
+      mockReq.query.email = 'email'
+      await require('../../controllers/contacts').listContacts(mockReq, mockRes)
+      expect(mockModels.Contact.findAll.mock.calls.length).toBe(1)
+      expect(mockModels.Contact.findAll.mock.calls[0][0].where).toEqual({
+        [Op.and]: [{ email: { [Op.like]: '%email%' } }]
+      })
+    })
+
+    it('should use and operator', async () => {
+      const Op = require('sequelize').Op
+      mockReq.query.operator = 'and'
+      mockReq.query.name = 'name'
+      mockReq.query.phone = '321'
+      mockReq.query.email = 'email'
+      await require('../../controllers/contacts').listContacts(mockReq, mockRes)
+      expect(mockModels.Contact.findAll.mock.calls.length).toBe(1)
+      expect(mockModels.Contact.findAll.mock.calls[0][0].where).toEqual({
+        [Op.and]: [
+          { name: { [Op.like]: '%name%' } },
+          { phone: { [Op.like]: '%321%' } },
+          { email: { [Op.like]: '%email%' } }
+        ]
+      })
+    })
+
+    it('should use or operator', async () => {
+      const Op = require('sequelize').Op
+      mockReq.query.operator = 'or'
+      mockReq.query.name = 'name'
+      mockReq.query.phone = '321'
+      mockReq.query.email = 'email'
+      await require('../../controllers/contacts').listContacts(mockReq, mockRes)
+      expect(mockModels.Contact.findAll.mock.calls.length).toBe(1)
+      expect(mockModels.Contact.findAll.mock.calls[0][0].where).toEqual({
+        [Op.or]: [
+          { name: { [Op.like]: '%name%' } },
+          { phone: { [Op.like]: '%321%' } },
+          { email: { [Op.like]: '%email%' } }
+        ]
+      })
     })
   })
 })
