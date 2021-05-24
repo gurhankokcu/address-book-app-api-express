42011af14ffa26eab57ff6919a88f9fbaff06677
Gurhan Kokcu <gurhankokcu@gmail.com>
2021-05-24 13:44:03 +0100

    enable cors for all routes

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 
 const config = require('config')
 const express = require('express')
+const cors = require('cors')
 const routes = require('./routes')
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 
--- tests/index.test.js
+++ tests/index.test.js
@@ -3,8 +3,9 @@
 describe('index', () => {
   const mockAppUse = jest.fn()
   const mockAppListen = jest.fn()
-  const mockJson = { key: 'value1' }
-  const mockRoutes = { key: 'value2' }
+  const mockCors = { key: 'value1' }
+  const mockJson = { key: 'value2' }
+  const mockRoutes = { key: 'value3' }
 
   beforeEach(() => {
     jest.mock('express', () => jest.fn().mockImplementation(() => {
@@ -13,20 +14,27 @@ describe('index', () => {
         listen: mockAppListen
       }
     }))
+    jest.mock('cors', () => () => mockCors)
     require('express').json = jest.fn().mockImplementation(() => mockJson)
     jest.mock('../routes', () => mockRoutes)
   })
 
+  it('should use cors', () => {
+    require('../index')
+    expect(mockAppUse.mock.calls.length).toBe(3)
+    expect(mockAppUse.mock.calls[0][0]).toBe(mockCors)
+  })
+
   it('should use body parser', () => {
     require('../index')
-    expect(mockAppUse.mock.calls.length).toBe(2)
-    expect(mockAppUse.mock.calls[0][0]).toBe(mockJson)
+    expect(mockAppUse.mock.calls.length).toBe(3)
+    expect(mockAppUse.mock.calls[1][0]).toBe(mockJson)
   })
 
   it('should use routes', () => {
     require('../index')
-    expect(mockAppUse.mock.calls.length).toBe(2)
-    expect(mockAppUse.mock.calls[1][0]).toBe(mockRoutes)
+    expect(mockAppUse.mock.calls.length).toBe(3)
+    expect(mockAppUse.mock.calls[2][0]).toBe(mockRoutes)
   })
 
   it('should listen port', () => {
